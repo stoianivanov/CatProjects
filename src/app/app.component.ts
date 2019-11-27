907f7092056bb22a0ccf8546f696b151d3c2d8b6
Stoian Ivanov <stoianivanovivanov@abv.bg>
2019-11-27 11:06:24 +0000

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Cat } from './cat.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'catDemo';
+  cats: Cat[] = [];
+  cat: Cat;
+  constructor(private http: HttpClient ) {
+  }
+
+  public giveMeCat() {
+    const url = 'https://api.thecatapi.com/v1/images/search';
+
+    this.http.get(url).subscribe((catResponse: HttpResponse<any>)=>{
+      console.log(catResponse);
+      this.cat = {
+        url: catResponse[0].url
+      }
+      this.cats.push(this.cat);
+      console.log(this.cats);
+    })
+  }
 }
